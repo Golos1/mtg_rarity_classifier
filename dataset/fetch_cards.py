ae997598bcaf394061a1891ce6b9116ebb79cf36
Golos1 <68024905+Golos1@users.noreply.github.com>
2025-07-02 14:13:00 -0400

    Fixed bulk fetching.

--- dataset/fetch_cards.py
+++ dataset/fetch_cards.py
@@ -1,17 +1,18 @@
 import json
+
 import requests
 from ratelimit import limits
 from tenacity import retry, wait_random
 
 @retry(wait=wait_random(60,80))
 @limits(calls=5, period=60)
-def get_bulk_rules()-> list[str] | None:
+def get_bulk_cards()-> list[str] | None:
     """
     Gets bulk rulings data from Scryfall's public API and returns rules text as an array of sentences. Method is rate limited on client side
     to avoid exceeding Scryfall API rates.
     :return: the embeddings.
     """
-    url = "https://api.scryfall.com/bulk-data/default_cards"
+    url = "https://api.scryfall.com/bulk-data/default-cards"
     result = requests.get(url)
     if result.status_code == 200:
         result = result.json()
@@ -19,7 +20,10 @@ def get_bulk_rules()-> list[str] | None:
         file_response = requests.get(result['download_uri'])
         if file_response.status_code == 200:
             content = json.loads(file_response.content)
-            rules: list[str] = [rule["comment"] for rule in content]
-            return rules
+            file = open("cards.json","w")
+            json.dump(content,file)
+            return content
         else:
             raise RuntimeError("Request returned code " + str(file_response.status_code))
+
+print(get_bulk_cards())
